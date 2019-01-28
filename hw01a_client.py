bef060b341071dd50663ff2e709b8f305e5a2775
edinpark <tonaco.braulio@gmail.com>
2019-01-27 21:20:12 -0500

    Renamed file

--- hw01a_client.py
+++ hw01a_client.py
@@ -1,16 +1,17 @@
 #   Client/Server Socket Interaction in UDP
 #   Author: Braulio Tonaco
 #   Date:   01/27/19
-
+#
 # STEP 1 - Create a Socket
-s = socket(AF_INET, SOCK_DGRAM)
-
+#   s = socket(AF_INET, SOCK_DGRAM)
+#
 # STEP 2 - Bind a socket
-s.bind((addressIP, addressPort))
-
+#   s.bind((addressIP, addressPort))
+#
 # STEP 3 - Exchange Data
-s.sendto(message.encode(), address)
-receivedMessage, address = s.recvfrom(2048)
-
+#   s.sendto(message.encode(), address)
+#   receivedMessage, address = s.recvfrom(2048)
+#
 # STEP 4 - Close socket
-close(s)
+#   close(s)
+
