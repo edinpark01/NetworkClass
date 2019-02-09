bd621eb5fc1e3982839727551550bb2b969136ba
edinpark01 <tonaco.braulio@gmail.com>
2019-02-09 18:58:03 -0500

    Renamed client + finished w/ prototype

--- HW02/client.py
+++ HW02/client.py
@@ -7,7 +7,7 @@
 #       To determine the RTT delay, the client records the time on sending a ping request to the server, and then
 #   records the time on receiving a ping response from the server.  The difference in the two times is the RTT.
 #
-#   The ping message contains 2 4-byte integers and must be encoded in network-byte order as follows :
+#   The ping message contains 2 4-byte integers and must be encoded in network-byte order as follows:
 #       •	4-byte message type with an integer value of 1 or 2
 #               o	Message type = 1 for a ping request (message from client to server)
 #               o	Message type = 2 for a ping response (message from server to client)
@@ -37,15 +37,60 @@
 #               o	Number of packets sent, received, lost (% loss rate)
 #               o	Min, Max, Average RTT for all acknowledged ping packets
 
-import struct
+import sys, time, struct
+from socket import *
 
-format_string = "!i"
 
-for num in range(0, 25):
-    packed = struct.pack(format_string, num)
-    print("Value {}\t\tPacket: {}".format(num, packed))
+def get_arguments():
+    """ Retrieves command line arguments """
 
-    unpacked = struct.unpack(format_string, packed)
-    print("Value {}\t\tPacket: {}".format(num, unpacked[0]))
-    print("\n")
+    arguments = {
+        "HOST": sys.argv[1],
+        "PORT": int(sys.argv[2]),
+        "MSG_TYPE": 1
+    }
+
+    return arguments
+
+# STEP 1: The client program will read in the above input parameters
+args = get_arguments()
+
+# STEP 2: Setup UDP protocol
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+# STEP 3: Sets timeout time (1 for one second)
+client_socket.settimeout(1)
+
+format_string = "!ii"
+
+print("Pinging {}, {}".format(args['HOST'], args['PORT']))
+
+# STEP 4: Send 10 ping requests consecutively to the server running at the specified IP address and port
+for msg_num in range(1, 11):
+    # STEP 4.0: Pack and Encode data
+    PACKED = struct.pack(format_string, args['MSG_TYPE'], int(msg_num))
+    DATA_LEN = sys.getsizeof(PACKED)
+
+    # STEP 4.1: Start timer
+    start = time.time()
+
+    # STEP 4.1: Sends request to server
+    client_socket.sendto(PACKED, (args['HOST'], args['PORT']))
+
+    # STEP 4.2: Wait for a response each time.
+    try:
+        data_echoed, address = client_socket.recvfrom(DATA_LEN)
+    except OSError:
+        print("Ping message number {} timed out".format(msg_num))
+        continue
+
+    # STEP 4.4: Record End + calculate RTT
+    end = time.time()
+    RTT = end - start
+
+    # STEP 4.5: Trace Output
+    print("Ping message number {} RTT: {} secs".format(msg_num, RTT))
+
+    # unpacked = struct.unpack(format_string, data_echoed)
+
 
